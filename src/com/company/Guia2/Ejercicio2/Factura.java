9fb0184f5edde279cf21d554796b3dd193479cc7
Nippur <54045811+nippur101@users.noreply.github.com>
2021-03-27 13:18:33 -0300

    agregar compra

--- src/com/company/Guia2/Ejercicio2/Factura.java
+++ src/com/company/Guia2/Ejercicio2/Factura.java
@@ -9,6 +9,9 @@ public class Factura {
     private double montoTotal;
     private LocalDate fecha;
     private Cliente cliente;
+    private ItemVenta [] item;
+    private int [] nroItems;
+    private int nroDeProdutos=0;
 
     public Factura() {
     }
@@ -18,9 +21,13 @@ public class Factura {
         this.fecha=LocalDate.now();
         this.idFactura = UUID.randomUUID().toString().toUpperCase().substring(0,6);
         this.montoTotal = montoTotal;
-
+        this.item=item;
+        this.nroItems=nroItems;
         this.cliente = cliente;
+        this.nroDeProdutos=nroDeProdutos;
     }
+
+
 
     public String getIdFactura() {
         return idFactura;
@@ -54,9 +61,55 @@ public class Factura {
         this.cliente = cliente;
     }
 
+    public ItemVenta[] getItem() {
+        return item;
+    }
+
+    public void setItem(ItemVenta[] item) {
+        this.item = item;
+    }
+
+    public int[] getNroItems() {
+        return nroItems;
+    }
+
+    public void setNroItems(int[] nroItems) {
+        this.nroItems = nroItems;
+    }
+
+    public int getNroDeProdutos() {
+        return nroDeProdutos;
+    }
+
+    public void setNroDeProdutos(int nroDeProdutos) {
+        this.nroDeProdutos = nroDeProdutos;
+    }
+
+    public void agregarCompra(ItemVenta it, int nroItems){
+
+         ItemVenta this.item[nroDeProdutos]=new ItemVenta();
+         item[nroDeProdutos]=it;
+        this.nroItems[nroDeProdutos]=nroItems;
+        this.nroDeProdutos++;
+
+    }
     public double montoFinal(){
         return montoTotal-montoTotal*cliente.getDescuento()/100;
     }
+
+    public double montoItems(int pos){
+        return item[pos].getPrecioUnitario()*nroItems[pos];
+    }
+    public double montoTotalItems(){
+        double suma =0;
+        for(int i=0;i<item.length;i++){
+            suma += item[i].getPrecioUnitario()*nroItems[i];
+        }
+
+        return suma;
+    }
+
+
 
 
     @Override
--- src/com/company/Guia2/Ejercicio2/Main.java
+++ src/com/company/Guia2/Ejercicio2/Main.java
@@ -4,8 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
     Cliente c1=new Cliente("Juan","juan@gmail.com",15);
+    ItemVenta i1=new ItemVenta( "001","jabon","Rexona",80.23);
+    ItemVenta i2=new ItemVenta( "002","Shampoo","head ands howlders",167.24);
+
     Factura f1=new Factura( 25368.05,c1);
+    f1.agregarCompra(i1,3);
+    f1.agregarCompra(i2,4);
         System.out.println(f1);
+        System.out.println(f1.montoTotalItems());
 
 
     }
