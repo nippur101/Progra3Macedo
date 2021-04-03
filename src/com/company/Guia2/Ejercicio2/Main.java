3eb107f987bb52dcf9ada5b3686c2702fa6101f1
Nippur <54045811+nippur101@users.noreply.github.com>
2021-04-03 11:22:01 -0300

    Ejercicio2Terminado2

--- src/com/company/Guia2/Ejercicio2/Main.java
+++ src/com/company/Guia2/Ejercicio2/Main.java
@@ -3,7 +3,7 @@ package com.company.Guia2.Ejercicio2;
 public class Main {
 
     public static void main(String[] args) {
-        Cliente c1=new Cliente("Juan","juan@gmail.com",15);
+        Cliente c1=new Cliente("Juan","juan@gmail.com",7);
         ItemVenta i1=new ItemVenta( "001","jabon","Rexona",80.23);
         ItemVenta i2=new ItemVenta( "002","Shampoo","head ands howlders",167.24);
         ItemVenta i3=new ItemVenta( "003","Crema de Enjuague","head ands howlders",150.20);
@@ -13,10 +13,10 @@ public class Main {
         f2.agregarCompra(i3,4);
         f1.agregarCompra(i1,3);
         f1.agregarCompra(i2,4);
-            System.out.println(f1);
-            System.out.println(f1.montoTotalItems());
-        System.out.println(f2.montoTotalItems());
 
+        System.out.println(f1.montoTotalItems());
+        System.out.println(f2.montoTotalItems());
+        System.out.println(f1);
 
     }
 }
