3633a6b153cf3af4697dc3c4f9154ef8fa6eac17
Nippur <54045811+nippur101@users.noreply.github.com>
2021-04-05 17:44:52 -0300

    Empezando guia3

---
+++ src/com/company/Guia3/Ejercicio1/Cilindro.java
@@ -0,0 +1,7 @@
+package com.company.Guia3.Ejercicio1;
+
+public class Cilindro extends Circulo {
+    private double altura;
+
+
+}
---
+++ src/com/company/Guia3/Ejercicio1/Main.java
@@ -0,0 +1,10 @@
+package com.company.Guia3.Ejercicio1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
